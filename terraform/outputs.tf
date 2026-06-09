48beafee7f4181c1997cae98f358bfbd7224584f
oliverangel45 <ollieangel10@gmail.com>
2026-06-09 10:20:11 +0100

    outputs.tf complete for terminal use

---
+++ terraform/outputs.tf
@@ -0,0 +1,31 @@
+# This file is used to verify the correct infrastructure has been created in the terminal after 'terraform apply'
+
+output "warehouse_name" {
+    value       = snowflake_warehouse.wc2026_wh.name
+    description = "Snowflake warehouse name"
+}
+
+output "database_name" {
+    value       = snowflake_database.wc2026.name
+    description = "Snowflake database name"
+}
+
+output "raw_schema" {
+    value       = "${snowflake_database.wc2026.name}.${snowflake_schema.raw.name}"
+    description = "Full path to RAW schema"
+}
+
+output "analytics_schema" {
+    value       = "${snowflake_database.wc2026.name}.${snowflake_schema.analytics.name}"
+    description = "Full path to ANALYTICS schema"
+}
+
+output "loader_role" {
+    value       = snowflake_role.loader.name
+    description = "Role used by Kafka consumer"
+}
+
+output "transformer_role" {
+    value       = snowflake_role.transformer.name
+    description = "Role used by dbt"
+}
