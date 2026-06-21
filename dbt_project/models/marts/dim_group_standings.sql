5c3ba3ec42cd5dac8f825cc5b3220f2b16b83077
oliverangel45 <ollieangel10@gmail.com>
2026-06-21 09:56:02 +0100

    group standings dimension complete with incremental logic

---
+++ dbt_project/models/marts/dim_group_standings.sql
@@ -0,0 +1,60 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key-'TEAM_ID'
+    )
+}}
+
+WITH standings AS (
+    SELECT * FROM {{ ref('stg_standings') }}
+),
+
+stadings_to_teams AS (
+    SELECT
+        s.TEAM_ID,
+        s.TEAM_NAME,
+        s.GROUP_NAME,
+        s.POSITION,
+        s.PLAYED_GAMES,
+        s.WON,
+        s.DRAW,
+        s.LOST,
+        s.POINTS,
+        s.GOALS_FOR,
+        s.GOALS_AGAINST,
+        s.GOAL_DIFF,
+        s.INGESTED_AT,
+        t.FLAG,
+        t.COUNTRY_CODE,
+        t.CONFEDERATION
+    FROM stadings s
+    LEFT JOIN {{ ref('teams') }} t ON s.TEAM_ID = t.TEAM_ID
+),
+
+fianl AS (
+    SELECT
+        TEAM_ID,
+        TEAM_NAME,
+        FLAG_EMOJI,
+        COUNTRY_CODE,
+        CONFEDERATION,
+        GROUP_NAME,
+        POSITION,
+        PLAYED_GAMES,
+        WON,
+        DRAW,
+        LOST,
+        POINTS,
+        GOALS_FOR,
+        GOALS_AGAINST,
+        GOAL_DIFF,
+        INGESTED_AT 
+    FROM stadings_to_teams
+    -- Incremental Logic
+    {% if is_incremental() %}
+    WHERE INGESTED_AT > (SELECT MAX(INGESTED_AT) FROM {{ this }})
+    {% endif %}
+)
+
+SELECT * FROM final
+ORDER BY GROUP_NAME, POSITION
