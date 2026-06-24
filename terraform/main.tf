0b9a1660a8c336fd0106a25528d5c02771f8cfd4
oliverangel45 <ollieangel10@gmail.com>
2026-06-24 23:17:36 +0100

    fix: dbt_ci syntax issue terraform step renamed

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "snowflake" {
     organization_name = var.snowflake_organization
     account_name      = var.snowflake_account
-    user          = var.snowflake_user
+    user              = var.snowflake_user
     password          = var.snowflake_password
     role              = "ACCOUNTADMIN"
     warehouse         = "COMPUTE_WH"
